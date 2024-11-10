845c386ca1ac80be72b2391e8f2c66c8fc15c09e
Fabrycio-Nakano <flnalmada@gmail.com>
2024-11-10 02:33:29 -0300

    smooth movement added

--- Martha_ws/src/martha_controller/scripts/wake_controller.py
+++ Martha_ws/src/martha_controller/scripts/wake_controller.py
@@ -1,75 +1,110 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 from std_msgs.msg import Float64
-import numpy as np
+import time
 
-l1 = 0.3  # Comprimento da coxa
-l2 = 0.3  # Comprimento da perna
-l_arm = 0.25  # Comprimento do braço superior
-l_forearm = 0.25  # Comprimento do antebraço
+def move_joint_smoothly(publisher, start_position, end_position, duration):
+    # Move a articulação suavemente de start_position para end_position em 'duration' segundos.
+    rate = 50  # Hz, número de atualizações por segundo
+    steps = rate * duration  # Número total de passos para completar o movimento
+    delta_position = (end_position - start_position) / steps  # Incremento por passo
 
-pub_hip_esquerdo = rospy.Publisher('/humanoid/quadril_esquerdo_rX_position_controller/command', Float64, queue_size=10)
-pub_hip_direito = rospy.Publisher('/humanoid/quadril_direito_rX_position_controller/command', Float64, queue_size=10)
-pub_knee_esquerdo = rospy.Publisher('/humanoid/joelho_esquerdo_rX_position_controller/command', Float64, queue_size=10)
-pub_knee_direito = rospy.Publisher('/humanoid/joelho_direito_rX_position_controller/command', Float64, queue_size=10)
-pub_shoulder_esquerdo = rospy.Publisher('/humanoid/ombro_esquerdo_rX_position_controller/command', Float64, queue_size=10)
-pub_shoulder_direito = rospy.Publisher('/humanoid/ombro_direito_rX_position_controller/command', Float64, queue_size=10)
-pub_elbow_esquerdo = rospy.Publisher('/humanoid/mao_esquerda_position_controller/command', Float64, queue_size=10)
-pub_elbow_direito = rospy.Publisher('/humanoid/mao_direita_position_controller/command', Float64, queue_size=10)
+    for i in range(int(steps)):
+        current_position = start_position + delta_position * i
+        publisher.publish(Float64(current_position))
+        time.sleep(1.0 / rate)  # Espera o tempo necessário para a próxima atualização
 
-def calcular_angulo_levantar_de_frente():
-    angulos_levantamento_frente = {
-        'hip': [-np.pi/4, -np.pi/6, 0, np.pi/6, np.pi/4],  # Ângulos do quadril
-        'knee': [np.pi/2, np.pi/3, np.pi/4, np.pi/6, 0],  # Ângulos do joelho
-        'shoulder': [-np.pi/4, -np.pi/6, 0, np.pi/6, np.pi/4],  # Ângulos do ombro
-        'elbow': [np.pi/4, np.pi/6, 0, -np.pi/6, -np.pi/4]  # Ângulos do cotovelo
-    }
-    return angulos_levantamento_frente
+def levantar_de_frente():
+    rospy.loginfo("Iniciando movimento de levantamento de frente...")
 
-def calcular_angulo_levantar_de_tras():
-    angulos_levantamento_tras = {
-        'hip': [np.pi/4, np.pi/6, 0, -np.pi/6, -np.pi/4],  # Ângulos do quadril
-        'knee': [np.pi/6, np.pi/4, np.pi/3, np.pi/2, np.pi],  # Ângulos do joelho
-        'shoulder': [np.pi/4, np.pi/6, 0, -np.pi/6, -np.pi/4],  # Ângulos do ombro
-        'elbow': [-np.pi/4, -np.pi/6, 0, np.pi/6, np.pi/4]  # Ângulos do cotovelo
-    }
-    return angulos_levantamento_tras
+    # Movimento para levantar o robô de frente usando move_joint_smoothly
+    move_joint_smoothly(pub_l_hip_pitch, 0.0, -1.0, 1)
+    move_joint_smoothly(pub_r_hip_pitch, 0.0, -1.0, 1)
 
-def publicar_angulos(angulos_levantamento):
-    rate = rospy.Rate(1)
-    for i in range(len(angulos_levantamento['hip'])):
-        pub_hip_esquerdo.publish(angulos_levantamento['hip'][i])
-        pub_hip_direito.publish(angulos_levantamento['hip'][i])
-        pub_knee_esquerdo.publish(angulos_levantamento['knee'][i])
-        pub_knee_direito.publish(angulos_levantamento['knee'][i])
-        
-        pub_shoulder_esquerdo.publish(angulos_levantamento['shoulder'][i])
-        pub_shoulder_direito.publish(angulos_levantamento['shoulder'][i])
-        pub_elbow_esquerdo.publish(angulos_levantamento['elbow'][i])
-        pub_elbow_direito.publish(angulos_levantamento['elbow'][i])
-        
-        rospy.loginfo(f"Publicando ângulos - Fase {i+1}")
-        rate.sleep()
+    move_joint_smoothly(pub_l_knee, 0.0, 1.5, 1)
+    move_joint_smoothly(pub_r_knee, 0.0, 1.5, 1)
 
-def main():
-    rospy.init_node('humanoid_lift_node')
+    move_joint_smoothly(pub_l_ankle_pitch, 0.0, -0.5, 1)
+    move_joint_smoothly(pub_r_ankle_pitch, 0.0, -0.5, 1)
 
+    move_joint_smoothly(pub_l_hip_pitch, -1.0, 0.0, 1)
+    move_joint_smoothly(pub_r_hip_pitch, -1.0, 0.0, 1)
+
+    rospy.loginfo("Robô levantado de frente.")
+
+def levantar_de_tras():
+    rospy.loginfo("Iniciando movimento de levantamento de trás...")
+
+    # Movimento para levantar o robô de trás usando move_joint_smoothly
+    move_joint_smoothly(pub_r_shoulder_pitch, 0.0, -1.5, 1)
+    move_joint_smoothly(pub_l_shoulder_pitch, 0.0, 1.5, 1)
+
+    move_joint_smoothly(pub_r_elbow, 0.0, -0.5, 1)
+    move_joint_smoothly(pub_l_elbow, 0.0, -0.5, 1)
+
+    move_joint_smoothly(pub_r_shoulder_pitch, -1.5, 0.0, 1)
+    move_joint_smoothly(pub_l_shoulder_pitch, 1.5, 0.0, 1)
+
+    move_joint_smoothly(pub_l_knee, 0.0, 1.5, 1)
+    move_joint_smoothly(pub_r_knee, 0.0, 1.5, 1)
+
+    move_joint_smoothly(pub_l_hip_pitch, 0.0, -1.0, 1)
+    move_joint_smoothly(pub_r_hip_pitch, 0.0, -1.0, 1)
+
+    move_joint_smoothly(pub_l_ankle_pitch, 0.0, 0.5, 1)
+    move_joint_smoothly(pub_r_ankle_pitch, 0.0, 0.5, 1)
+
+    move_joint_smoothly(pub_l_hip_pitch, -1.0, 0.0, 1)
+    move_joint_smoothly(pub_r_hip_pitch, -1.0, 0.0, 1)
+
+    rospy.loginfo("Robô levantado de trás.")
+
+# A função set_joint_positions permanece inalterada
+def set_joint_positions():
+    rospy.init_node('humanoid_lift_node', anonymous=True)
+
+    global pub_r_shoulder_pitch, pub_r_shoulder_roll, pub_r_elbow, pub_l_shoulder_pitch, pub_l_shoulder_roll, pub_l_elbow
+    global pub_r_hip_yaw, pub_r_hip_roll, pub_r_hip_pitch, pub_r_knee, pub_r_ankle_pitch, pub_r_ankle_roll
+    global pub_l_hip_yaw, pub_l_hip_roll, pub_l_hip_pitch, pub_l_knee, pub_l_ankle_pitch, pub_l_ankle_roll
+
+    # Publishers para as articulações do braço e pernas
+    pub_r_shoulder_pitch = rospy.Publisher('/martha/r_sho_pitch_position/command', Float64, queue_size=10)
+    pub_r_shoulder_roll = rospy.Publisher('/martha/r_sho_roll_position/command', Float64, queue_size=10)
+    pub_r_elbow = rospy.Publisher('/martha/r_el_position/command', Float64, queue_size=10)
+
+    pub_l_shoulder_pitch = rospy.Publisher('/martha/l_sho_pitch_position/command', Float64, queue_size=10)
+    pub_l_shoulder_roll = rospy.Publisher('/martha/l_sho_roll_position/command', Float64, queue_size=10)
+    pub_l_elbow = rospy.Publisher('/martha/l_el_position/command', Float64, queue_size=10)
+
+    pub_r_hip_yaw = rospy.Publisher('/martha/r_hip_yaw_position/command', Float64, queue_size=10)
+    pub_r_hip_roll = rospy.Publisher('/martha/r_hip_roll_position/command', Float64, queue_size=10)
+    pub_r_hip_pitch = rospy.Publisher('/martha/r_hip_pitch_position/command', Float64, queue_size=10)
+    pub_r_knee = rospy.Publisher('/martha/r_knee_position/command', Float64, queue_size=10)
+    pub_r_ankle_pitch = rospy.Publisher('/martha/r_ank_pitch_position/command', Float64, queue_size=10)
+    pub_r_ankle_roll = rospy.Publisher('/martha/r_ank_roll_position/command', Float64, queue_size=10)
+
+    pub_l_hip_yaw = rospy.Publisher('/martha/l_hip_yaw_position/command', Float64, queue_size=10)
+    pub_l_hip_roll = rospy.Publisher('/martha/l_hip_roll_position/command', Float64, queue_size=10)
+    pub_l_hip_pitch = rospy.Publisher('/martha/l_hip_pitch_position/command', Float64, queue_size=10)
+    pub_l_knee = rospy.Publisher('/martha/l_knee_position/command', Float64, queue_size=10)
+    pub_l_ankle_pitch = rospy.Publisher('/martha/l_ank_pitch_position/command', Float64, queue_size=10)
+    pub_l_ankle_roll = rospy.Publisher('/martha/l_ank_roll_position/command', Float64, queue_size=10)
+
+    rospy.sleep(1)
+
+    # Escolher o movimento 
     movimento = input("Digite 'frente' para levantar de frente ou 'tras' para levantar de trás: ")
 
     if movimento == 'frente':
-        angulos_frente = calcular_angulo_levantar_de_frente()
-        rospy.loginfo("Levantar de frente...")
-        publicar_angulos(angulos_frente)
+        levantar_de_frente()
     elif movimento == 'tras':
-        angulos_tras = calcular_angulo_levantar_de_tras()
-        rospy.loginfo("Levantar de trás...")
-        publicar_angulos(angulos_tras)
+        levantar_de_tras()
     else:
         rospy.loginfo("Movimento inválido.")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
-        main()
+        set_joint_positions()
     except rospy.ROSInterruptException:
         pass
