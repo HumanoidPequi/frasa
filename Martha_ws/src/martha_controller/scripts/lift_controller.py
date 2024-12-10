adfe68c0a052688ea273ae6c49bba188ff299903
lucasolives <lucasolives31@gmail.com>
2024-12-10 02:07:08 -0300

    changing orientation and type of messaged

--- Martha_ws/src/martha_controller/scripts/lift_controller.py
+++ Martha_ws/src/martha_controller/scripts/lift_controller.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import rospy
-from std_msgs.msg import Float64, Int16MultiArray
+from std_msgs.msg import Float64, Float64MultiArray
 import time
 def move_joints_smoothly(joints, start_positions, end_positions, duration):
     rate = 50  # Hz
@@ -21,7 +21,7 @@ def move_joints_smoothly(joints, start_positions, end_positions, duration):
         if not simulation_mode:
             # Publica os arrays combinados
             for array_name in arrays:
-                msg = Int16MultiArray()
+                msg = Float64MultiArray()
                 msg.data = arrays[array_name]
                 array_publishers[array_name].publish(msg)
         time.sleep(1.0 / rate)
@@ -48,6 +48,7 @@ def levantar_de_tras():
     end_positions_step3 = [0.0, 0.0]
     move_joints_smoothly(joints_step3, start_positions_step3, end_positions_step3, 3)
     rospy.loginfo("Robô levantado em T-pose.")
+
 def levantar_de_frente():
     rospy.loginfo("Iniciando movimento de levantamento de frente...")
     # Passo 1: Girar ombro e ajustar antebraço para posição de apoio
@@ -61,28 +62,29 @@ def levantar_de_frente():
     rospy.loginfo("Passo 2: Empurrando o chão, dobrando joelhos e ajustando tornozelos...")
     joints_step2 = ['r_shoulder_pitch', 'l_shoulder_pitch', 'r_knee', 'l_knee', 'r_hip_pitch', 'l_hip_pitch', 'r_ankle_pitch', 'l_ankle_pitch']
     start_positions_step2 = [-1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    end_positions_step2 = [2.0, -2.0, 1.7, 1.7, 1.5, 1.5, 1.2, 1.2]
+    end_positions_step2 = [2.0, -2.0, 1.7, 1.7, -1.5, -1.5, 1.2, 1.2]
     move_joints_smoothly(joints_step2, start_positions_step2, end_positions_step2, 2)
     rospy.sleep(3)
     # Passo 2.5: Retornar cotovelos para posição 0.0 e dobrar mais os joelhos
     rospy.loginfo("Passo 2.5: Retornando cotovelos para posição 0.0 e dobrando mais os joelhos...")
     joints_step2_5 = ['r_elbow', 'l_elbow', 'r_knee', 'l_knee']
     start_positions_step2_5 = [-2.0, -2.0, 1.7, 1.7]
-    end_positions_step2_5 = [0.0, 0.0, 2.5, 2.5]
+    end_positions_step2_5 = [0.0, 0.0, 2.0, 2.0]
     move_joints_smoothly(joints_step2_5, start_positions_step2_5, end_positions_step2_5, 1)
     rospy.sleep(1)
     # Passo 3: Levantar o tronco e assumir a T-pose
     rospy.loginfo("Passo 3: Levantando o tronco para a T-pose...")
     joints_step3 = ['r_hip_pitch', 'l_hip_pitch', 'r_knee', 'l_knee', 'r_shoulder_roll', 'l_shoulder_roll', 'r_shoulder_pitch', 'l_shoulder_pitch', 'r_elbow', 'l_elbow', 'r_ankle_pitch', 'l_ankle_pitch']
-    start_positions_step3 = [1.5, 1.5, 2.5, 2.5, 0.0, 0.0, 2.0, -2.0, 0.0, 0.0, 1.2, 1.2]
+    start_positions_step3 = [-1.5, -1.5, 2.0, 2.0, 0.0, 0.0, 2.0, -2.0, 0.0, 0.0, 1.2, 1.2]
     end_positions_step3 = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
     move_joints_smoothly(joints_step3, start_positions_step3, end_positions_step3, 3)
     rospy.loginfo("Robô levantado em T-pose.")
+
 def set_joint_positions():
     rospy.init_node('humanoid_lift_node', anonymous=True)
     global simulation_mode
-    simulation_input = input("Digite 'sim' se estiver usando a simulação, ou 'nao' caso contrário: ")
-    simulation_mode = (simulation_input.lower() == 'sim')
+    simulation_input = input("Digite 'y' se estiver usando a simulação, ou 'n' caso contrário: ")
+    simulation_mode = (simulation_input.lower() == 'y')
     global joint_publishers, joint_array_mapping, arrays, array_publishers
     # Publishers individuais (simulação)
     joint_publishers = {}
@@ -107,10 +109,10 @@ def set_joint_positions():
     else:
         # Publishers combinados
         array_publishers = {}
-        array_publishers['right_leg'] = rospy.Publisher('/martha/right_leg/command', Int16MultiArray, queue_size=10)
-        array_publishers['left_leg'] = rospy.Publisher('/martha/left_leg/command', Int16MultiArray, queue_size=10)
-        array_publishers['right_arm'] = rospy.Publisher('/martha/arm_r/command', Int16MultiArray, queue_size=10)
-        array_publishers['left_arm_head'] = rospy.Publisher('/martha/arm_l_head/command', Int16MultiArray, queue_size=10)
+        array_publishers['right_leg'] = rospy.Publisher('/marta/right_leg/command', Float64MultiArray, queue_size=10)
+        array_publishers['left_leg'] = rospy.Publisher('/marta/left_leg/command', Float64MultiArray, queue_size=10)
+        array_publishers['right_arm'] = rospy.Publisher('/marta/arm_r/command', Float64MultiArray, queue_size=10)
+        array_publishers['left_arm_head'] = rospy.Publisher('/marta/arm_l_head/command', Float64MultiArray, queue_size=10)
         # Inicializa os arrays de posições
         arrays = {}
         arrays['right_leg'] = [0]*6
@@ -122,11 +124,11 @@ def set_joint_positions():
             'r_shoulder_pitch': ('right_arm', 0),
             'r_shoulder_roll': ('right_arm', 1),
             'r_elbow': ('right_arm', 2),
-            'l_shoulder_pitch': ('left_arm_head', 3),
-            'l_shoulder_roll': ('left_arm_head', 4),
-            'l_elbow': ('left_arm_head', 5),
-            'neck_yaw': ('left_arm_head', 1),
-            'neck_pitch': ('left_arm_head', 2),
+            'l_shoulder_pitch': ('left_arm_head', 2),
+            'l_shoulder_roll': ('left_arm_head', 3),
+            'l_elbow': ('left_arm_head', 4),
+            'neck_yaw': ('left_arm_head', 0),
+            'neck_pitch': ('left_arm_head', 1),
             'r_hip_pitch': ('right_leg', 2),
             'r_hip_roll': ('right_leg', 1),
             'r_knee': ('right_leg', 3),
@@ -138,10 +140,10 @@ def set_joint_positions():
         }
         rospy.sleep(1)
     # Escolher o movimento
-    movimento = input("Digite 'frente' para levantar de frente ou 'tras' para levantar de trás: ")
-    if movimento == 'frente':
+    movimento = input("Digite 'f' para levantar de frente ou 't' para levantar de trás: ")
+    if movimento == 'f':
         levantar_de_frente()
-    elif movimento == 'tras':
+    elif movimento == 't':
         levantar_de_tras()
     else:
         rospy.loginfo("Movimento inválido.")
